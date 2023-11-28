370a9c895a4c31f3ee71591545bd438ff19d21e5
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-11-28 15:09:46 +0300

    init-commit

---
+++ src/utilities/sum.test.js
@@ -0,0 +1,11 @@
+const sum = require("./sum")
+
+describe("sum", () => {
+  test("Корректное значение", () => {
+    expect(sum(2, 3)).toBe(5)
+  })
+
+  test("Корректное значение", () => {
+    expect(sum(0, 0)).toBe(0)
+  })
+})
