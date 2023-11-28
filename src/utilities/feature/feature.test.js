10151eb14157e94524a08b85bc672f3698b41224
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-11-28 16:19:00 +0300

    Create feature.test.js

---
+++ src/utilities/feature/feature.test.js
@@ -0,0 +1,11 @@
+const minus = require("./feature")
+
+describe("minus", () => {
+  test("Корректное значение", () => {
+    expect(minus(2, 3)).toBe(-1)
+  })
+
+  test("Корректное значение", () => {
+    expect(minus(0, 0)).toBe(2)
+  })
+})
